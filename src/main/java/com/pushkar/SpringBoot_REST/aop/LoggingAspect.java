b292e7722bfcab023b0d2446c96754bf767f39f3
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-06-30 20:24:04 +0530

    Different Advices Implemented , which can be used to observe the Application working , without affecting Business Model

--- src/main/java/com/pushkar/SpringBoot_REST/aop/LoggingAspect.java
+++ src/main/java/com/pushkar/SpringBoot_REST/aop/LoggingAspect.java
@@ -1,7 +1,7 @@
 package com.pushkar.SpringBoot_REST.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,9 +13,23 @@ public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
                  //return type, class-name.method-name(args)  --format of below stars
-    @Before("execution(* com.pushkar.SpringBoot_REST.service.JobService.*(..))")
-    public void logMethodCall(){
-        LOGGER.info("Method Called");
+    @Before("execution(* com.pushkar.SpringBoot_REST.service.JobService.updateJob(..))")
+    public void logMethodCall(JoinPoint jp){
+        LOGGER.info("Method Called :- "+ jp.getSignature().getName());
     }
 
+    @After("execution(* com.pushkar.SpringBoot_REST.service.JobService.getJob(..))")
+    public void logMethodExecuted(JoinPoint jp){
+        LOGGER.info("Method Executed :- "+ jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.pushkar.SpringBoot_REST.service.JobService.getJob(..))")
+    public void logMethodCrash(JoinPoint jp){
+        LOGGER.info("Method Has Some Issues :- "+ jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.pushkar.SpringBoot_REST.service.JobService.getJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint jp){
+        LOGGER.info("Method Executed Successfully :- "+ jp.getSignature().getName());
+    }
 }
