498da795d616cce4ca27aecf0b7b60daa8fe6ed7
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2024-06-30 12:02:04 +0530

    Applying Spring-AOP Concepts

---
+++ src/main/java/com/pushkar/SpringBoot_REST/aop/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.pushkar.SpringBoot_REST.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+                 //return type, class-name.method-name(args)  --format of below stars
+    @Before("execution(* com.pushkar.SpringBoot_REST.service.*(..)")
+    public void logMethodCall(){
+        LOGGER.info("Method Called");
+    }
+
+}
